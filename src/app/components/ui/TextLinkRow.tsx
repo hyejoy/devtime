019edb1001a969ca91aa1912a0c4505c5a60f8b4
hyejoy <devchoi97@gmail.com>
2026-01-23 22:29:45 +0900

    refactor(ui): prepare base structure for compound components

--- src/app/components/ui/TextLinkRow.tsx
+++ src/app/components/ui/TextLinkRow.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link';
 import styles from './TextLinkRow.module.css';
 import classNames from 'classnames/bind';
+
 type RoutePath = `/${string}`;
-type TextLink = {
+
+interface Props {
   question?: string;
   label: string;
   href: RoutePath;
   isBold?: boolean;
-};
+}
+
 const cx = classNames.bind(styles);
 
 export default function TextLinkRow({
@@ -15,12 +18,11 @@ export default function TextLinkRow({
   label,
   href,
   isBold = false,
-}: TextLink) {
-  const bold = isBold ? 'bold' : 'notBold';
+}: Props) {
   return (
     <div className={cx('linkContainer')}>
-      <div>{question}</div>
-      <Link className={cx(bold)} href={href}>
+      {question && <div>{question}</div>}
+      <Link href={href} className={cx({ bold: isBold, notBold: !isBold })}>
         {label}
       </Link>
     </div>
