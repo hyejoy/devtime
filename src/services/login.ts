ba0db264491917357208c03ff9f56cf43ae24348
hyejoy <devchoi97@gmail.com>
2026-01-23 19:13:08 +0900

    feat(api-core): add login service and remove unused types

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,14 +1,11 @@
-// services/signup.ts
 import { API } from '@/constants/endpoints';
 import { fetcher } from '@/lib/fetcher';
+import { LoginRequest, LoginResponse } from '@/types/api';
 
-import { LoginInput, LoginResponse } from '@/types/login';
-import { NextApiResponse } from 'next';
-
-/** 이메일 중복확인 */
-export async function login(payload: LoginInput) {
-  return fetcher<NextApiResponse<LoginResponse>>(API.AUTH.LOGIN, {
+/** 로그인 */
+export const login = (data: LoginRequest): Promise<LoginResponse> => {
+  return fetcher(`${API.AUTH.LOGIN}`, {
     method: 'POST',
-    body: JSON.stringify(payload),
+    body: data,
   });
-}
+};
