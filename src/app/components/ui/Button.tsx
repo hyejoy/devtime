5e235ebcf8da71b550cf40eec4b6d352033c39d3
hyejoy <devchoi97@gmail.com>
2026-01-23 16:33:28 +0900

    feat(button): apply classnames bind (cx) to Button component

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -5,12 +5,12 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'primary' | 'secondary' | 'tertiary';
 };
 
+const cx = classNames.bind(styles);
 export default function Button({
   variant = 'primary',
   className = '',
   ...props
 }: ButtonProps) {
-  const cx = classNames.bind(styles);
   return (
     <button
       className={cx('button', `variant_${variant}`, className)}
