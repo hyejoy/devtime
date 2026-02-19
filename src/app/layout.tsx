9403918c11704f5224028e5b56005e79eb8faf48
hyejoy <devchoi97@gmail.com>
2026-02-19 17:47:41 +0900

    chore: 사이트 메타데이터 및 Open Graph 설정 추가
    - layout.tsx에 title, description, og-image 설정 적용
    - 카카오톡, 슬랙 등 링크 공유 시 미리보기 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,34 @@ import { useProfileStore } from '@/store/profileStore';
 import { usePathname } from 'next/navigation';
 import { useDialogStore } from '@/store/dialogStore';
 import NextTopLoader from 'nextjs-toploader';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'DevTime - 개발 학습 시간 관리',
+  description: '개발 공부 시간을 측정하고 기록하는 타이머 기반 웹 애플리케이션',
+  openGraph: {
+    title: 'DevTime',
+    description: '나의 성장을 기록하는 시간, DevTime',
+    url: 'https://devtime-chi.vercel.app',
+    siteName: 'DevTime',
+    images: [
+      {
+        url: 'https://devtime-chi.vercel.app/images/og-image.png', //
+        width: 1200,
+        height: 630,
+        alt: 'DevTime 서비스 미리보기 이미지',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DevTime',
+    description: '나의 성장을 기록하는 시간, DevTime',
+    images: ['https://devtime-chi.vercel.app/images/og-image.png'],
+  },
+};
 //   fontFamily: 'var(--font-pretendard)',
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
